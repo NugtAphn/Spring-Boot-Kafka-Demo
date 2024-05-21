c1aa022eb1b0e1f46ac50584c734ff36b1c0245b
Tung <sneakiedward@gmail.com>
2024-05-21 10:06:57 +0700

    first push

---
+++ src/test/java/net/nugtaphn/springbootkafkademo/SpringbootKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nugtaphn.springbootkafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
